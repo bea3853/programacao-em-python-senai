903263030304539557977816016cdd650942c9bc
Beatriz Alves <55098795+bea3853@users.noreply.github.com>
2026-08-25 17:24:27 -0300

    Refactor name variable extraction logic
    Refactor variable handling to extract name from formatted string.

--- Aula-4/mostrando_nome_variavel.py
+++ Aula-4/mostrando_nome_variavel.py
@@ -2,6 +2,10 @@ name = "Beatriz"
 nome   =  f"{name=}" 
 print(nome[0:4])
 
+name = "Beatriz"
+nome_ = f'{name = }'
+nome_var = nome_.split()[0]
+print(nome_var) 
 
 name = "Beatriz"
 nome_ = f'{name = }'
